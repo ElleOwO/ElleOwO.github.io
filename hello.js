28211e0a2c9ab028028d8200fe6e2ca474e952c3
ElleOwO <lmtdmya@gmail.com>
2024-02-18 03:43:06 -0600

    created about page with nav animations

--- hello.js
+++ hello.js
@@ -3,6 +3,9 @@ function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("open").style.zIndex = "0";
     document.getElementById("close").style.zIndex = "99999";
+    document.getElementById("return-icon").style.animation= "fadeIn 1s";
+
+
 
 }
 
@@ -11,4 +14,6 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("open").style.zIndex = "99999";
     document.getElementById("close").style.zIndex = "0";
+    document.getElementById("return-icon").style.animation= "none!;";
+
 }
